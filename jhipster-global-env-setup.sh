e19420560c08775374b159eadbb4dffc39939e8f
Amjad Hossain <amjad.hossain.rahat@gmail.com>
2021-05-11 12:19:25 -0700

    added new commands in env setup script

--- jhipster-global-env-setup.sh
+++ jhipster-global-env-setup.sh
@@ -44,7 +44,19 @@ echo "Going to install/confirm .NET Core Runtime 3.1 (LTS)..."
 echo ""
 sudo apt install -y dotnet-runtime-3.1
 
+echo "Going to install .NET Framework 5.0 (Latest)..."
+echo ""
+sudo apt install -y dotnet-sdk-5.0
+
+echo "Going to install/confirm .NET Core runtime 5.0 (lates)..."
+echo ""
+sudo apt install -y dotnet-runtime-5.0
+
+
 dotnet --version
+dotnet --list-sdks
+dotnet --list-runtimes
+
 
 echo "Going to install jhipster .net blueprint..."
 echo ""
